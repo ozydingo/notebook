66a7fede736c6b3737978e1fd964557a9efd4c72
Andrew Schwartz <andrew.schwartz@brain-power.com>
2019-09-21 16:11:27 -0400

    docker/workers: App running with redis and rq connections

--- code/docker/workers-rq/services/web/app/app.py
+++ code/docker/workers-rq/services/web/app/app.py
@@ -1,25 +1,32 @@
+import sys, os, socket, json
 from flask import Flask, request
 from redis import Redis, RedisError
-import sys
-import os
-import socket
 from rq import Queue
+from rq.connections import NoRedisConnectionException
+
+import my_worker
 
 app = Flask(__name__)
 
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+redis = Redis(host="redis", db=0, socket_connect_timeout=10, socket_timeout=10)
 
 @app.route("/")
 def hello():
-    q = Queue(redis);
-    q.enqueue(redis.decr, "counter"))
-
     try:
-        visits = redis.get("counter")
-    except RedisError:
-        visits = "<i>cannot connect to Redis, counter disabled</i>"
+        queue = Queue(connection=redis);
+        queue.enqueue(my_worker.work)
+        response = {
+            'success': True,
+            'message': "Work enqueued",
+        }
+    except (RedisError, NoRedisConnectionException) as err:
+        print(err)
+        response = {
+            'success': True,
+            'message': str(err),
+        }
 
-    return f'<div>{visits}</div><p>Hello, world'
+    return json.dumps(response);
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
--- code/docker/workers-rq/services/worker/lib/my_worker.py
+++ code/docker/workers-rq/services/worker/lib/my_worker.py
@@ -1,8 +1,9 @@
+import logging
 import time
 
 def work(n=10):
     print("Going to work!")
     for ii in range(n):
-        print(f'{ii}: working...'')
+        print(f"{ii}: working...")
         time.sleep(1)
     print("Done!")
