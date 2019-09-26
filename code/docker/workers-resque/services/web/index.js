c2d63171b8633dcfaa037d1fe14ab6aa6e67e565
Andrew Schwartz <andrew.schwartz@brain-power.com>
2019-09-26 07:41:43 -0400

    docker/workers-resque add express static path and request logging middleware

--- code/docker/workers-resque/services/web/index.js
+++ code/docker/workers-resque/services/web/index.js
@@ -3,11 +3,27 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+function middle(func) {
+  return (req, res, next) => {
+    logRequest(req, res);
+    next();
+  }
+}
+
+function logRequest(req, res) {
+  console.log(`Started ${req.method} for ${req.url}`);
+  console.log('Request headers:', req.headers);
+  console.log('Requuest body:', req.body);
+  console.log('Request query:', req.query);
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(middle(logRequest))
+app.use(express.static(__dirname));
 
-app.get('/', (req, res) => res.send('Hello, world!'));
 app.post('/', (req, res) => {
   res.send(req.body);
 })
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
