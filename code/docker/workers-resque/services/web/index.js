51ee9e72c23d057ddede32cea13c52994176a036
Andrew Schwartz <andrew.schwartz@brain-power.com>
2019-09-26 03:00:22 -0400

    docker/workers-reqsuue: Install and set up barebones express server

---
+++ code/docker/workers-resque/services/web/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.send('Hello, world!'));
+app.post('/', (req, res) => {
+  res.send(req.body);
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
